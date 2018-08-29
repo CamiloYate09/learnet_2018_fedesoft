f6bc2623efbb8fdaf7665da6e265afde51ec8d00
camilo Yate <cyateya35439@universidadean.edu.co>
2018-08-29 03:03:38 -0500

    completando frontend

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Learnet' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Learnet'});
+});
+
+//ruta para login
+router.get('/Login', function (req, res, next) {
+    res.render('Login', {title: 'Login'});
+});
+
+//ruta para registrarse
+//UserRegister
+router.get('/UserRegister', function (req, res, next) {
+    res.render('UserRegister', {title: 'UserRegister'});
 });
 
 module.exports = router;
